ddb1e9138272d47527e6db5dc3c7f7014f2d2c53
Carlos Guisao Franco <titofranco@gmail.com>
2013-08-15 00:38:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -61,7 +61,7 @@ sudo rm /etc/apt/sources.list.d/*java*
 sudo apt-get update
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java7-installer
+sudo apt-get install oracle-java7-installer -y
 sudo apt-get install openjdk-7-jre-headless -y
 
 #Imagemagick dependencies
