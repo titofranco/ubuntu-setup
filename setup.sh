b18cd810b23ff93fb19a4a6aced7fbd4e8d8c7c7
Carlos Guisao Franco <titofranco@gmail.com>
2013-08-30 15:51:32 -0500

    Added postgresql dependencie

--- setup.sh
+++ setup.sh
@@ -48,6 +48,9 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 #Mysql server
 sudo apt-get -y install mysql-server libmysqlclient-dev
 
+#Postgresql
+sudo apt-get -y libpq-dev
+
 #Mongodb
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
