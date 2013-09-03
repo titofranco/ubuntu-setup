6fcc66da3caa6f862ecbe01dd8d9df6954629205
Carlos Guisao Franco <titofranco@gmail.com>
2013-09-03 18:39:25 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -89,6 +89,8 @@ wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
 make
+sudo cp src/redis-server /usr/local/bin/
+sudo cp src/redis-cli /usr/local/bin/
 cd $HOME
 
 #Wine
